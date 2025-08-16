030175e6e88451d49733403e4ef9d057e8eed8fc
RajneeshYadav-123 <rajneeshyadav0507@gmail.com>
2025-08-17 00:08:49 +0530

    build ready

--- Backend/app.js
+++ Backend/app.js
@@ -48,10 +48,11 @@ app.get('/', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
 
 
 
+module.exports = app;
